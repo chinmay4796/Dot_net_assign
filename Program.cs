1da1fac952c0839dd14f0dabb27c7a07e796c503
chinmay4796 <68509885+chinmay4796@users.noreply.github.com>
2020-12-09 01:01:01 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,125 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employee
+namespace LambdaAssign
 {
     class Program
     {
         static void Main()
         {
-            Employee o1 = new Employee("Amol", 123465, 10);
-            Employee o2 = new Employee("Amol", 123465);
-            Employee o3 = new Employee("Amol");
-            Employee o4 = new Employee();
+            // 1. simple Interest
+            Func<decimal, decimal, decimal, decimal> SimpleInterest = (p, n, r) => n * p * r;
+            Console.WriteLine("Simple Interest : "+ SimpleInterest(20000,4,5));
 
-            //Employee o1 = new Employee();
-            //Employee o2 = new Employee();
-            //Employee o3 = new Employee();
-            Console.WriteLine(o1.EmpNo);
-            Console.WriteLine(o2.EmpNo);
-            Console.WriteLine(o3.EmpNo);
-            Console.WriteLine(o4.EmpNo);
+            // 2. greater
+            Func<int, int, bool> isGreater = (a, b) => a > b;
+            Console.WriteLine("a > b =>"+ isGreater(6,4));
 
+            //3
+            Func<int, bool> Even = (a) => a % 2 == 0;
+            Console.WriteLine("No. is Even =>"+ Even(4));
+
+            // 4.isGreaterThan10000
+            Employee e = new Employee();
+            //Func<Employee, bool> o = (a) => e.basicSalary > 10000;
+
+            //Employee e = new Employee() { basicSalary = 12000 };
+
+            Predicate<Employee> o = (emp) => e.basicSalary > 10000;
+            Console.WriteLine("isGreater than 10000"+o(e));
             Console.ReadLine();
+
 
         }
     }
 
-    class Employee
+    public class Employee
     {
-        private static int count;
-        private string name;
-        public string Name
-        {
-            set
-            {
-                if (!String.IsNullOrWhiteSpace(value))
-                {
-                    name = value;
-                }
-                else
-                {
-                    Console.WriteLine("PLEASE ENTER VALID NAME...");
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                }
-            }
-            get
-            {
-                return name;
-            }
-        }
-        private int empNo;
-        public int EmpNo
-        {
-            get
-            {
-                return empNo;
-            }
-        }
-
-        private decimal basic;
-        public decimal Basic
-        {
-            set
-            {
-                if (value >= 10000 && value <= 200000)
-                {
-                    basic = value;
-                }
-                else
-                {
-                    Console.WriteLine("invalid basic salary");
-                }
-            }
-        }
-        private short deptNo;
-        public short DeptNo
-        {
-            set
-            {
-                if (value > 0)
-                {
-                    deptNo = value;
-                }
-                else
-                {
-                    Console.WriteLine("Enter valid department number...");
-                }
-            }
-        }
-        public Employee(string name, decimal basic, short deptNo)
-        {
-            Name = name;
-            Basic = basic;
-            DeptNo = deptNo;
-            empNo = ++count;
-            Console.WriteLine("no->" + empNo);
-        }
-
-        public Employee(string name, decimal basic)
-        {
-            Name = name;
-            Basic = basic;
-            empNo = ++count;
-            Console.WriteLine("no->" + empNo);
-        }
-        public Employee(string name)
-        {
-            Name = name;
-            empNo = ++count;
-            Console.WriteLine("no->" + empNo);
-        }
+        public int basicSalary = 40000;
 
         public Employee()
         {
-            empNo = ++count;
-            Console.WriteLine("no param constructor....");
+
         }
 
-        public decimal GetNetSalary()
-        {
-            return basic + 1200;
-        }
+
     }
-
 }
